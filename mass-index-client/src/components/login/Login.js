9c25699f947c4c172bc7edc8debec16b381cfb66
nisa <nurunnisa9@gmail.com>
2021-12-20 00:09:37 +0300

    login with axios completed

--- mass-index-client/src/components/login/Login.js
+++ mass-index-client/src/components/login/Login.js
@@ -1,24 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import "./login.css"
+const axios = require('axios').default;
 
 const Login = () => {
-    return(
-    <form>
-        <div className="form-inner">
-            <h2>LOGİN</h2>
-            {/* ERROR! */}
-            <div className="form-group">
-                <label htmlFor="email">Email:</label>
-                <input type="email" name="email" id="email"/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password:</label>
-                <input type="password" name="password" id="password"/>
-            </div>
-            <input type="submit" value="LOGİN"/><input type="submit" value="REGİSTER"/>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+
+    function handleSubmit(event) {
+    axios.post('http://localhost:3000/user/login', {
+        password: password,
+        email: email
+    })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+        event.preventDefault();
+
+    }
+
+
+    function logınRequest() {
+
+    }
+
+
+    return (
+        <div className="Login">
+            <Form onSubmit={handleSubmit}>
+                <Form.Group size="lg" controlId="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        autoFocus
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group size="lg" controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Form.Group>
+                <Button block size="lg" type="submit" disabled={!validateForm()}>
+                    Login
+                </Button>
+            </Form>
         </div>
-    </form>
-    )
+    );
 }
 
 export default Login
