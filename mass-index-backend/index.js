3f213c9c6d6a88d54487208b2c7f3140ac2c41e7
nisa <nurunnisa9@gmail.com>
2021-12-20 00:09:17 +0300

    cors and body-parser added

--- mass-index-backend/index.js
+++ mass-index-backend/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express()
 const mongoose = require('mongoose');
 const port = 3000
+const cors = require('cors')
+const bodyParser = require('body-parser');
 
+app.use(cors())
+app.use(bodyParser.json());
 app.use(express.json())
+
 mongoose.connect('mongodb://localhost:27017/member');
 
 const userRouter = require('./routes/userRouter');
