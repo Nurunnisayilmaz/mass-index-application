ea98f8f0231f0931645ab4ae8d86dd4b1ae8c58b
nisa <nurunnisa9@gmail.com>
2022-01-14 19:25:03 +0300

    Editing the register page

--- mass-index-client/src/components/register/Register.js
+++ mass-index-client/src/components/register/Register.js
@@ -1,40 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import "./register.css"
+const axios = require('axios').default;
+
 
 const Register = () => {
-    return(
-        <form>
-            <div className="form-inner">
-                <h2>REGİSTER</h2>
-                {/* ERROR! */}
-                <div className="form-group">
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" name="email" id="email"/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" name="password" id="password"/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="name">Name:</label>
-                    <input type="name" name="name" id="name"/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="surname">Surname:</label>
-                    <input type="surname" name="surname" id="surname"/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="weight">Weight:</label>
-                    <input type="weight" name="weight" id="weight"/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="height">Height:</label>
-                    <input type="height" name="height" id="height"/>
-                </div>
-                <input type="submit" value="SAVE"/><input type="submit" value="LOGİN"/>
-            </div>
-        </form>
-    )
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [surname, setSurname] = useState("");
+    const [weight, setWeight] = useState("");
+    const [height, setHeight] = useState("");
+
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+
+    function handleSubmit(event) {
+        axios.post('http://localhost:3000/register', {
+            password: password,
+            email: email,
+            name: name,
+            surname: surname,
+            weight: weight,
+            height: height
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+        event.preventDefault();
+
+    }
+
+    function registerRequest() {
+
+    }
+
+    return (
+        <div className="Register">
+            <Form onSubmit={handleSubmit}>
+                <Form.Group size="lg" controlId="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        autoFocus
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group size="lg" controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group size="lg" controlId="name">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        type="name"
+                        value={name}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group size="lg" controlId="surname">
+                    <Form.Label>Surname</Form.Label>
+                    <Form.Control
+                        type="surname"
+                        value={surname}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group size="lg" controlId="weight">
+                    <Form.Label>Weight</Form.Label>
+                    <Form.Control
+                        type="weight"
+                        value={weight}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group size="lg" controlId="height">
+                    <Form.Label>Height</Form.Label>
+                    <Form.Control
+                        type="height"
+                        value={height}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Form.Group>
+                <Button block size="lg" type="submit" disabled={!validateForm()}>
+                    Save
+                </Button>
+                <Button block size="lg" type="submit" disabled={!validateForm()}>
+                    Login
+                </Button>
+            </Form>
+        </div>
+    );
 }
 
 export default Register
